a71631d9f142ceb5451af8be7652fe43b5fdf28a
jh3389 <jh3389@columbia.edu>
2015-08-29 23:18:57 -0400

    verification from history detail

--- iacuc/update_kaput.py
+++ iacuc/update_kaput.py
@@ -2,7 +2,9 @@
 
 import sys
 
-import db_connector, jproperties
+import db_connector
+import jproperties
+import javaobj
 
 SQL_SUBMIT_TASKID="select A.TASKID_, O.STATUSCODE from IACUC_MIGRATOR A, IACUCPROTOCOLSTATUS O\
  where a.STATUSID_=O.OID and O.STATUSCODE='Submit'\
@@ -26,6 +28,24 @@ SQL_SUBMIT_TASKID="select A.TASKID_, O.STATUSCODE from IACUC_MIGRATOR A, IACUCPR
       )\
     )\
 )"
+
+SQL_HI_DETAIL = "\
+    select bytes_ from ACT_GE_BYTEARRAY where ID_= (\
+      select BYTEARRAY_ID_ from ACT_HI_DETAIL where TASK_ID_ = :task_id)"
+
+def print_detail(task_id):
+    cursor = db_connector.DBConnector.cursor()
+    cursor.prepare(SQL_HI_DETAIL)
+    cursor.execute(None, {'task_id': task_id})
+    res = cursor.fetchone()
+
+    if res is not None:
+        blob_data = res[0]
+        pobj = javaobj.loads(blob_data.read())
+        print(pobj)
+
+    cursor.close
+
 
 def get_submission_task_id():
     cursor = db_connector.DBConnector.cursor()
@@ -56,11 +76,12 @@ def main():
 
     submission_task_id = get_submission_task_id()
     print('num of submission: {}'.format(len(submission_task_id)))
-    close_db()
 
     for task_id in submission_task_id:
         print('task_id={}'.format(task_id))
+        print_detail(task_id)
 
+    close_db()
     return 0
 
 if __name__ == '__main__':
