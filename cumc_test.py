f82139ab20465f4bbd030a2ae578a88622e8f072
jh3389 <jh3389@columbia.edu>
2015-09-01 20:53:50 -0400

    verification from history detail & update kaput to submit

--- cumc_test.py
+++ cumc_test.py
@@ -24,6 +24,24 @@ where\
   A.ARCHIVE='N' and A.ACTIVE='Y' \
   and P.PROTOCOLNUMBER='AAAJ7852'"
 
+SQL_ATTACHED_CONSET_FORM="\
+select H.OID, P.PROTOCOLNUMBER, H.protocolYear, H.modificationnumber,\
+A.FILENAME, A.DOCUMENTDATA, A.DOCUMENTDATASTAMPED, A.DOCUMENTIDENTIFIER\
+  from IRBPROTOCOLHEADER H\
+  join IRBPROTOCOL P on H.PARENTPROTOCOLOID=P.OID\
+  join IRBSTATUS S on S.PARENTOBJECTOID=H.OID\
+  join IRBATTACHMENT A on A.IRBPROTOCOLHEADERID=H.OID\
+  where\
+  H.OID = (select max(iph.OID) from IrbProtocolHeader iph\
+  where iph.PARENTPROTOCOLOID = H.PARENTPROTOCOLOID) and\
+  S.OID = (select max(ss.OID) from IrbStatus ss where ss.PARENTOBJECTOID=H.OID) and\
+  H.IRBAPPROVALDATE is not null and\
+  trunc(H.EXPIRATIONDATE) >= trunc(sysdate) and\
+  S.STATUSNAME='Approved' and\
+  A.ARCHIVE='N' and A.ACTIVE='Y' and\
+  A.ATTACHMENTTYPECODE in (2, 3, 10, 26)\
+  and P.PROTOCOLNUMBER='AAAK5904'"
+
 DIR_NAME = "/tmp/cumc/"
 
 def download_protocol():
@@ -46,8 +64,27 @@ def download_protocol():
 
     cursor.close
 
-def dump_to_file(file_name, blob_data):
+def download_attached_consent_form():
+    cursor = db_connector.DBConnector.cursor()
+    cursor.execute(SQL_ATTACHED_CONSET_FORM)
 
+    for res in cursor:
+        protocol_num = '{}_Y{:02}_M{:02}'.format(res[1], res[2], res[3])
+        print(protocol_num)
+        dir_name = DIR_NAME + '/' + protocol_num + '/ATTACHED_CONSENT_FORM'
+        if not os.path.exists(dir_name):
+            os.makedirs(dir_name)
+
+        file_name = dir_name + '/' + res[4]
+
+        if res[6] is not None:
+            dump_to_file(file_name, res[6])
+        else:
+            dump_to_file(file_name, res[5])
+
+    cursor.close
+
+def dump_to_file(file_name, blob_data):
     try:
         file = open(file_name, 'w')
         file.write(blob_data.read())
@@ -64,7 +101,10 @@ def main():
         print('error: connection string undefined')
         return 1
     db_connector.DBConnector(connection_str)
+
     download_protocol()
+    download_attached_consent_form()
+
     db_connector.DBConnector.close()
     return 0
 
