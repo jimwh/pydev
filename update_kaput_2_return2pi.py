418605a9c0256433d0219bb2c5faed8c2bbc38c6
jh3389 <jh3389@columbia.edu>
2015-12-29 15:46:27 -0500

    from kaput to  pi

---
+++ update_kaput_2_return2pi.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+
+import sys
+
+from db import db_connector
+from db import jproperties
+
+SQL_TASK_ID = "select HT.ID_\
+  from ACT_HI_TASKINST HT, ACT_HI_PROCINST HP, IACUCPROTOCOLSTATUS S\
+  where HT.task_def_key_='kaput'\
+  and HT.PROC_DEF_ID_ like 'IacucApprov%'\
+  and HT.PROC_INST_ID_ = HP.PROC_INST_ID_\
+  and HT.DELETE_REASON_='completed'\
+  and S.IACUCPROTOCOLHEADERPER_OID = HP.BUSINESS_KEY_\
+  and S.STATUSCODEDATE = HT.END_TIME_\
+  and trunc(S.STATUSCODEDATE) >= trunc(to_date('01/01/2015', 'MM/DD/YYYY'))\
+  and S.STATUSCODE='ReturnToPI'\
+  order by HT.ID_ desc"
+
+SQL_UPDATE = "update ACT_HI_TASKINST\
+    set TASK_DEF_KEY_='returnToPI', NAME_='Return to PI'\
+    where ID_= :task_id and TASK_DEF_KEY_='kaput'"
+
+
+def update_kaput_to_return2pi(task_id):
+    cursor = db_connector.DBConnector.cursor()
+    cursor.prepare(SQL_UPDATE)
+    cursor.execute(None, {'task_id': task_id})
+    db_connector.DBConnector.commit()
+    cursor.close()
+
+
+def get_task_id():
+    task_id = []
+    cursor = db_connector.DBConnector.cursor()
+    cursor.execute(SQL_TASK_ID)
+    for res in cursor:
+        task_id.append(res[0])
+    cursor.close()
+    return task_id
+
+
+def main():
+    prop = jproperties.Properties()
+    prop.load("./db/db.properties")
+    if len(sys.argv) == 1:
+        print('Usage: %s <connection string>' % sys.argv[0])
+        sys.exit(1)
+
+    connection_str = prop.get(sys.argv[1])
+    if connection_str is None:
+        print('No connection string defined')
+        sys.exit(1)
+
+    db_connector.DBConnector(connection_str)
+
+    task_id_list = get_task_id()
+    print("listSize=%d" % len(task_id_list))
+
+    for task_id in task_id_list:
+        print("id=%s" % task_id)
+        # update_kaput_to_return2pi(task_id)
+
+    db_connector.DBConnector.close()
+    sys.exit(0)
+
+if __name__ == '__main__':
+    main()
